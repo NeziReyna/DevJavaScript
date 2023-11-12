71444b47e29780487a69b6a8dbbba167a6486e7f
NeziReyna <principenezi@gmail.com>
2023-11-12 14:32:52 -0800

    CreatPost

--- javascript/main.js
+++ javascript/main.js
@@ -2,3 +2,8 @@ let getLogin = document.getElementById("loginButton")
 getLogin.addEventListener("click", () =>{
   window.open("views/login.html")
 });
+
+let getCreatePost = document.getElementById("createPostButton");
+getCreatePost.addEventListener("click", () => {
+  window.open("views/createPost.html", "_blank");
+});
