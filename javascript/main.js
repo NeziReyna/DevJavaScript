f5996496d10d4770e5788785fabd7da6647d4928
Alejandro Sanchez <alexchicha23@gmail.com>
2023-11-12 20:41:13 -0600

    feat: create structure Post

--- javascript/main.js
+++ javascript/main.js
@@ -2,8 +2,3 @@ let getLogin = document.getElementById("loginButton")
 getLogin.addEventListener("click", () =>{
   window.open("views/login.html")
 });
-
-let getCreatePost = document.getElementById("createPostButton");
-getCreatePost.addEventListener("click", () => {
-  window.open("views/createPost.html", "_blank");
-});
